1caa46f573b772588b5200c9079ff6165c69c591
Blagoja <baze_sarkisjan@yahoo.com>
2026-07-30 18:41:19 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React from "react";
 // import ReactDOM from 'react-dom';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './App.js';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from "./App.js";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
-root.render (
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+root.render(
+  /* КЛУЧНАТА ПОПРАВКА: Му кажуваме на рутерот дека почетната точка на GitHub Pages е /fit_life */
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>,
 );
